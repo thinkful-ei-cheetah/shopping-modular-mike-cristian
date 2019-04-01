0c74244999b1cad641cb41725d914a0e35623a16
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-04-01 11:34:42 -0700

    updated CRUD

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,5 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  
 });
--- scripts/store.js
+++ scripts/store.js
@@ -14,9 +14,43 @@ const store = (function () {
   const hideCheckedItems = false;
   const searchTerm = '';
 
+  const findById = items.find(id => items.id === id);
+  const addItem = function(name) {
+    try {
+        Item.validateName(name);
+        const newItem = Item.create(name); 
+        this.items.push(newItem);
+    }
+    catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  }
+  const findAndToggleChecked = function(id) {
+      const foundItem = this.findById(id);
+      foundItem.checked = !foundItem.checked;
+  }
+  const findAndUpdateName = function(id, newName) {
+      try {
+          Item.validateName(newName);
+          const item = findById(id);
+          item.name = newName;
+      }
+      catch(error) {
+        console.log('Cannot add item: ' + error.message);
+      }
+  }
+  const findAndDelete = function(id) {
+      this.items.splice(this.items.findIndex(item => item.id === id), 1);
+  }
+
   return {
     items,
     hideCheckedItems,
     searchTerm,
+    findById, 
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 }() );
