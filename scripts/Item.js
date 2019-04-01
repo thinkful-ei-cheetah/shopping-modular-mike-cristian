ac02eb5805ae3e5ced1b577b019191acdf957fc6
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-04-01 09:28:52 -0700

    item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -2,9 +2,23 @@
 /* global shoppingList, cuid, $ */
 
 const Item = (function() {
+  const validateName = function(name) {
+    if (!name) {
+      throw TypeError('Name does not exist.')
+    }
+  }
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  }
   
   return { 
-
+    validateName,
+    create,
   };
 
 }() );
